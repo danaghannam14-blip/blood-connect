ac481eb42057442f2b055944f344d13d48103f65
admin <danaghannam14@gmail.com>
2026-05-02 23:04:10 +0300

    remove resend use nodemailer

--- backend/routes/requests.js
+++ backend/routes/requests.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const db = require('../db');
-const nodemailer = require('nodemailer');
+const { Resend } = require('resend');
 
 const transporter = nodemailer.createTransport({
   service: 'gmail',
