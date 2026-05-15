54a0c38614c051a9d5e571b914bd683c9c844dcb
admin <danaghannam14@gmail.com>
2026-05-15 21:38:04 +0300

    Fix: Configure vite to handle react-leaflet

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,7 +2,15 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  plugins: [
-    react(),
-  ],
+  plugins: [react()],
+  build: {
+    rollupOptions: {
+      external: ['react-leaflet'],
+      output: {
+        globals: {
+          'react-leaflet': 'ReactLeaflet'
+        }
+      }
+    }
+  }
 })
