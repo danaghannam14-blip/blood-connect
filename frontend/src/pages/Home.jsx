e9e2592dffef8e17bd7ae1d3b14326110622a1fc
admin <danaghannam14@gmail.com>
2026-05-21 03:56:06 +0300

    update center's location in Home

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -579,7 +579,7 @@ export default function Home() {
               </div>
 
               <div style={{ marginTop:'clamp(12px,1.5vw,20px)', paddingTop:'clamp(12px,1.5vw,20px)', borderTop:'2px solid rgba(211,47,47,.15)' }}>
-                <a href="https://www.google.com/maps/place/33.89652171663661,35.483144430563314/@33.89652171663661,35.483144430563314,18z" target="_blank" rel="noopener noreferrer"
+                <a href="https://www.google.com/maps/place/33.896527,35.483139/@33.896527,35.483139,18z" target="_blank" rel="noopener noreferrer"
                   className="bc-btn bc-btn-primary"
                   style={{ width:'100%', padding:'clamp(12px,1.5vw,16px)', borderRadius:16, fontSize:'clamp(10px,1.1vw,12px)', fontWeight:900, display:'flex', alignItems:'center', justifyContent:'center', gap:8, textDecoration:'none' }}>
                   <span>📍</span>
