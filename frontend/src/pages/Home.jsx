ddd364913e4d0bdf171d27e8f1df80fdbf54d410
admin <danaghannam14@gmail.com>
2026-05-28 17:44:10 +0300

    Set YouTube video to HD1440p quality

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1284,8 +1284,7 @@ export default function Home() {
     <iframe
       width="100%"
       height="100%"
-      src="https://www.youtube.com/embed/MGsZUvVrOtg?vq=hd1440"
-      title="BloodConnect"
+src="https://www.youtube.com/embed/MGsZUvVrOtg?vq=hd1440&autoplay=1&mute=1"      title="BloodConnect"
       frameBorder="0"
       allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
       allowFullScreen
